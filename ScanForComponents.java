319159ba7bffa88486e3dcac90a64a2fb9d51c0f
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2024-11-15 09:59:52 +0530

    Scans and registers
    Scans and registers all annotated components @Component` @Service etc

---
+++ ScanForComponents.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Component;
+
+@Component 
+public class MyComponent {
+    public void doSomething() {
+        System.out.println("Component logic executed.");
+    }
+}
